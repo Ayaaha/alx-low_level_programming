816023993c119354c2b0cfdf8ea23786fe16f626
Aya <aysa109@gmail.com>
2023-08-13 08:41:57 -0700

    (2-play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:alphabet in lowercase
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+
+	char alpha = 'a';
+	int i;
+
+	for (i = 1, i < 26, i++)
+		int putchar(alpha);
+	return (0);
+}
