8171d8e6201ab4825fb968a1a1db0eef977e7066
Aya <aysa109@gmail.com>
2023-08-13 09:48:11 -0700

    3-alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:alphabet in lowercase
+ *
+ * Return:  0 (Success)
+ */
+
+int main(void)
+{
+
+	char lower, upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	putchar('\n');
+	return (0);
+}
