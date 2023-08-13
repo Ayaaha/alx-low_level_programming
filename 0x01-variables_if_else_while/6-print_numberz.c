970e008961f95c10f27b05c1bdcfcd234a244c1e
Aya <aysa109@gmail.com>
2023-08-13 10:11:44 -0700

    6-Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:alphabet in low prints all single digit numbers
+ *			of base 10 starting from 0
+ *			using putchar function
+ *
+ * Return:  0 (Success)
+ */
+
+int main(void)
+{
+
+	int numz;
+
+	for (numz = 0; numz <= 9; numz++)
+	{
+		putchar(int numz);
+	}
+		putchar('\n');
+	return (0);
+}
