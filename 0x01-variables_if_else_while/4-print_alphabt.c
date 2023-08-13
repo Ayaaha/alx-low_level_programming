b5b8808e1d12b0273ec282bd80e2d962b078a42a
Aya <aysa109@gmail.com>
2023-08-13 09:52:57 -0700

    4-I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:alphabet in lowercase except q and e
+ *
+ * Return:  0 (Success)
+ */
+
+int main(void)
+{
+
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && 'e');
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
