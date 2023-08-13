24f121529ee55f73efd2a92e6bdecdd06e590361
Aya <aysa109@gmail.com>
2023-08-13 11:54:58 -0700

    8- Hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: all possible combinations of single-digit numbers
+ *
+ * Return:  0 (Success)
+ */
+
+int main(void)
+{
+
+	int cb;
+
+	for (cb = 0; cb <= 9; cb++)
+	{
+		putchar('0' + cb);
+		putchar('0' + ',');
+	}
+		putchar('\n');
+	return (0);
+}
