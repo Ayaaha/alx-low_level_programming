27abdfc1931519ce338a4917a25c4c253ca5281c
Aya <aysa109@gmail.com>
2023-08-13 15:37:10 -0700

    10-The more brains you use,

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,22 +11,22 @@
 int main(void)
 {
 
-	char a, b, c, d;
+	char a, b;
 
 	a = 0;
 
-do {
 	for (b = 0; b <= 9; b++)
 	{
-		putchar('0' + b);
+		for (a = 0; a <= 9; a++)
+		{
+			if (a == b)
+			continue;
 
-	for (c = 0; c <= 9; c++)
-		putchar('0' + c);
-	}
+		putchar('0' + b);
 		putchar('0' + a);
 		putchar(44);
 		putchar(' ');
-}
-		while (a <= 9);
+		}
+	}
 	return (0);
 }
