536b5de9b4bf435796b9318d75f90e196df42e56
Aya <aysa109@gmail.com>
2023-08-14 04:05:51 -0700

    12-Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,14 +18,14 @@ int main(void)
 		for (b = 0; b <= 9; b++)
 		{
 		if (a != b && a < b)
-		putchar('0' + a);
-		putchar('0' + b);
-		putchar(' ');
 		for (c = 0; c <= 9; c++)
 		{
 		for (d = 0; d <= 9; d++)
 		{
 		if (c != d && c < d)
+		putchar('0' + a);
+		putchar('0' + b);
+		putchar(' ');
 		putchar('0' + c);
 		putchar('0' + d);
 		if (a == 9 && b == 8 && c == 9 && d == 9)
