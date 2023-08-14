3f06abc4ab5eeb7e667ba639fe4db158599a221a
Aya <aysa109@gmail.com>
2023-08-13 17:35:33 -0700

    11- Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry Point
  *
- * Description: all possible different combinations of three digits
+ * Description: all possible different combinations of two digits
  *
  * Return:  0 (Success)
  */
@@ -13,22 +13,23 @@ int main(void)
 
 	int a, b, c;
 
-	a = 0;
-
-	for (b = 0; b <= 9; b++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (a = 0; a <= 9; a++)
+		for (b = 1; b <= 9; b++)
 		{
-		for (c = 0; c <= 9; c++)
-			{
-		if (a != b && a < b)
-		putchar('0' + b);
+		for (c = 2; c <= 9; c++)
+		if ((a != b) && (a < b))
+		{
 		putchar('0' + a);
+		putchar('0' + b);
 		putchar('0' + c);
+		if (a == 7 && b == 8 && c == 9)
+		break;
 		putchar(44);
 		putchar(' ');
-			}
+		}
 		}
 	}
+		putchar('\n');
 	return (0);
 }
