2c659d0a6de713d918a59739c520cba6e5264f3e
Aya <aysa109@gmail.com>
2023-08-15 16:35:20 -0700

    1-call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase
+ *
+ * Return: 0 Success
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
+	_putchar(alpha);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void);
+
+#endif
