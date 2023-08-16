8e204d5dd4075f7a730d24e82e70eb9d70dfc61e
Aya <aysa109@gmail.com>
2023-08-16 16:04:55 -0700

    11- 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,9 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-	{
 	for (count = n; n > 98; count--)
 	printf("%d, ", count);
-	}
 		else
-		{
 		for (count = n; n < 98; count++)
 		printf("%d, ", count);
-		}
 }
-
