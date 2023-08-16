f0da91cf6da8f59b80609a30c1c9caad41ef0d0c
Aya <aysa109@gmail.com>
2023-08-16 10:21:22 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,24 +4,24 @@
  *print_sign  - prints the sign of a number
  * @n: sign to be checked
  *
- * Return: 1 Success
+ * Return: 1 if  '+', 0 if 0, -1 if '-'
  */
 int print_sign(int n)
 {
 	if (n  > 0)
 	{
-	return (1);
 	_putchar('+');
+	return (1);
 	}
 		else if (n == 0)
 		{
-		return (0);
 		_putchar('0');
+		return (0);
 		}
 			else if (n < 0)
 			{
-			return (-1);
 			_putchar('-');
+			return (-1);
 			}
 }
 
