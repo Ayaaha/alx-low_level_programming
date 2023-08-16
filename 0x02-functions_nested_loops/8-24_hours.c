3e964269c317267305bc0c87d130b26fdfc679ec
Aya <aysa109@gmail.com>
2023-08-16 12:28:55 -0700

    8-federal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	for (hr = 0; hr <= 23; hr++)
 	{
-		for (min = 0; min <= 23; min++)
+		for (min = 0; min <= 59; min++)
 		{
 		_putchar(48 + hr / 10);
 		_putchar(48 + hr % 10);
