05e8170e9d0bec038cefcab41be5dbc0f5a7d3a5
Aya <aysa109@gmail.com>
2023-08-16 13:37:14 -0700

    9-9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,14 @@ void times_table(void)
 {
 	int r, c, t;
 
-	for (r = 0; r <= 9; r++)
+	for (r = 0; r < 9; r++)
 	{
-		for (c = 0; c <= 9; c++)
+		for (c = 0; c < 9; c++)
 		{
 			t = r * c;
 			_putchar('0' + t);
 			_putchar(',');
-			_putchar(' ');
+			_putchar('  ');
 		}
 			_putchar('\n');
 	}
