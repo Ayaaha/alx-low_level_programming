581c3a53de826658839acedec71906cab54ae1d6
Aya <aysa109@gmail.com>
2023-08-17 16:52:21 -0700

    2- 0 > 972 ?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * largest_number - prints the largest of 3 integers
+ * @i: positive or negative parameter
+ */
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
 
 #endif /* MAIN_H */
