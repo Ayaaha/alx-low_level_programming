bede0ec0b51035710f00e212731d5bdc0a1c5db5
Aya <aysa109@gmail.com>
2023-08-18 14:45:13 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: character to be checked
+ *
+ * Return: 1 Success
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+	else
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,10 +8,12 @@
  * _putchar - Print character to the output
  * Return: int value
  * _isupper - checks for uppercase character
+ *  _isgigit  - checks for a digit
  */
 
 int main(void);
 int _putchar(char ch);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
