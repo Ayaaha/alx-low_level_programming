68a01842b3d378b12803f3a66b62c2465ad84f34
Aya <aysa109@gmail.com>
2023-08-18 15:05:38 -0700

    2- Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * @a: The result of multiplication
+ *
+ * Return: 1 Success
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,11 +9,12 @@
  * Return: int value
  * _isupper - checks for uppercase character
  *  _isgigit  - checks for a digit
+ *  mul  - multiply two integar numbers
  */
 
 int main(void);
 int _putchar(char ch);
 int _isupper(int c);
 int _isdigit(int c);
-
+int mul(int a, int b);
 #endif
