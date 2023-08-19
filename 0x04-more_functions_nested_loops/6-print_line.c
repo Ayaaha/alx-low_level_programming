ca8f4ac8391a6da1f6b2dc9f731e5ecd1a66a9f5
Aya <aysa109@gmail.com>
2023-08-18 17:25:47 -0700

    6-a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,8 +12,10 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (count = 0; count <= n ; count++)
+		for (count = 1; count <= n ; count++)
+		{
 			_putchar('_');
+		}
 			_putchar('\n');
 	}
 				else
