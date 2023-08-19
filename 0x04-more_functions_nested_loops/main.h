06a3d5373dfa930b7d2fa15a79910548da0a7083
Aya <aysa109@gmail.com>
2023-08-18 17:00:04 -0700

    6-a straight line

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,6 +13,7 @@
  *  print_numbers - prints the numbers from 0 to 9
  *  print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
  *  more_numbers - prints 10 times the numbers, from 0 to 14
+ *  print_line - draw stright line
  */
 
 int main(void);
@@ -23,5 +24,5 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
+void print_line(int n);
 #endif
