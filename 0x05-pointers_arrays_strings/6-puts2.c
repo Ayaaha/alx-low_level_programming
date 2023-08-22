05782b7d5aa91b89b680eae975e9a038ca150bab
Aya <aysa109@gmail.com>
2023-08-21 18:57:20 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts2 -   prints  a string
+ * puts2 -   prints  a string
  * @str:string
  * Return: 1 Success
  */
 
-void _puts2(char *str)
+void puts2(char *str)
 {
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str+2;
+		str = str + 2;
 	}
 	_putchar('\n');
 }
