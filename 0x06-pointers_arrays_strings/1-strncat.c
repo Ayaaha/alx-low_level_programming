3c189885574f9fdccb9aa8d4db9fbe9a36fdec9f
Aya <aysa109@gmail.com>
2023-08-23 15:46:58 -0700

    1- concatenates two strings- n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 		for (i = 0 ; i < n && src[i] != '\0'; i++)
 		{
 			dest[len + i] = src[i];
-			dest[len + i] = '\0';
 		}
+			dest[len + i] = '\0';
 		return (dest);
 }
