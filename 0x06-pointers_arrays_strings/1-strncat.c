c907c06873111ca51f479b8e58d1be5adc179e51
Aya <aysa109@gmail.com>
2023-08-23 13:41:39 -0700

    0-concatanation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * *_strcat -  appends the src string to the dest string
+ * *_strncat -  appends the src string to the dest string
  * @dest: destination string
  * @src: string
+ * @n: n bytes
  * Return: 1 Success
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 		size_t i, len;
 
 		len = strlen(dest);
-		for (i = 0 ; i < src[i] != '\0'; i++)
+		for (i = 0 ; i < n && src[i] != '\0'; i++)
 			dest[len + i] = src[i];
 			dest[len + i] = '\0';
 		return (dest);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,10 +10,12 @@
  * *_strcat -  appends the src string to the dest string
  * @*src:the string pointed to by src
  * @*dest: destination pointer
+ * *_strncat -  appends the src string to the dest string
+ * @*n: n bytes
  */
 
 int main(void);
 int _putchar(char ch);
 char *_strcat(char *dest, char *src);
-
+char *_strncat(char *dest, char *src, int n);
 #endif
