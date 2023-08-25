db92b6fe9a00b51c92f37a8e4d6be076771ec415
Aya <aysa109@gmail.com>
2023-08-24 17:11:04 -0700

    6-capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,22 @@ char *cap_string(char *orig)
 {
 	int i;
 	char c;
-
 	char *a = orig;
+
 	i = 0;
 
+	if (a[i] != '\0')
+	{
+		a[i] = toupper(a[i]);
+	}
 	while (a[i] != '\0')
 	{
+		c = a[i];
 		switch (c)
 		{
-			case ',':
-			case ';':
-			case '.':
+			case ' ':
+			case '\t':
+			case '\n':
 			case '!':
 			case '?':
 			case '"':
@@ -28,12 +33,14 @@ char *cap_string(char *orig)
 			case ')':
 			case '{':
 			case '}':
-			case '\n':
-			case '\t':
-			case ' ':
-				a[i] = toupper(a[i]);
-			default:
-			a[i] = a[i];
+			case ',':
+			case ';':
+			case '.':
+				if (a[i + 1] != '\0' && isalpha(a[i + 1]))
+				{
+					a[i + 1] = toupper(a[i + 1]);
+				}
+				break;
 		}
 	i++;
 	}
