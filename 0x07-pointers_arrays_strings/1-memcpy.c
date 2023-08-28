9983b29afca13f9c2c693678ba383bca7907ef8d
Aya <aysa109@gmail.com>
2023-08-28 13:19:30 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_memcyp -  fills memory with a constant byte
+ * @dest: the destination memory area 
+ * @n: number of bytes
+ * @src: source string
+ * Return: 1 Success
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *res = memcpy(dest, src, n);
+			return (res);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,10 +9,13 @@
  * _putchar - prints character in the  output
  * *_memset -  fills memory with a constant byte
  * @*s: the memory area pointed to by s
+ * *_memcpy - copies memory area
+ * @*dest: destination memory area
+ * @*src: source memory area
  */
 
 int main(void);
 int _putchar(char ch);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
