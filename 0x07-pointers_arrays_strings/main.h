97fb827f18c7ee921257e991e42f3198820740d6
Aya <aysa109@gmail.com>
2023-08-28 13:38:17 -0700

    2-locates a character in a string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,10 +12,12 @@
  * *_memcpy - copies memory area
  * @*dest: destination memory area
  * @*src: source memory area
+ * *_strchr - function that locates a character in a string
  */
 
 int main(void);
 int _putchar(char ch);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
