2ece909e0ab0539a2c205202946aa13320e4b3a3
Aya <aysa109@gmail.com>
2023-09-01 00:20:47 +0000

    100- palindorme string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_palindrome -  returns 1 if a string is a palindrome and 0 if not
+ * @sb: begining of the string
+ * @sd: ending of the string
+ * Return: 1 Success
+ */
+
+
+int palindrome(char *sb, char *sd);
+
+int is_palindrome(char *s)
+{
+	char *s1;
+	int len;
+	
+	len = strlen(s);
+	s1 = s + len - 1;
+
+	return (palindrome(s, s1));
+}
+
+/**
+ * palindrome - helper recursion function
+ * @sb: begining of the string
+ * @sd: ending of the string
+ * Return: 1 Success
+ *
+ */
+
+int palindrome(char *sb, char *sd)
+{
+	if (*sb == 0 && *sd == 0)
+	{
+	return (1);
+	}
+	else if (sb >= sd)
+	{
+	return (1);
+	}
+	else if (sb != sd)
+	{
+	return (0);
+	}
+	else
+	{
+	return (palindrome(sb + 1, sd - 1));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,9 @@
  * _sqrt_recursion - eturns the natural square root of a number
  * square - helper recursion function
  * is_prime_number -  returns 1 if the input integer is a prime number
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * wildcmp - unction that compares two strings and returns 1
+ * if the strings can be considered identical, otherwise
  */
 
 int main(void);
@@ -28,5 +31,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int v);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
