8d300aa29803b0251a3b1bc72227718ce50d91be
Aya <aysa109@gmail.com>
2023-09-01 03:59:13 +0000

    101- wildcompare

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,14 +9,6 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
-	{
-	if (*s1 != '\0')
-	{
-	return (1);
-	}
-	return (wildcmp(s1 + 1, s2 + 1));
-	}
 	if (*s2 == '*')
 	{
 	if (*(s2 + 1) == '*')
@@ -25,8 +17,13 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
-	else
+	else if (*s1 == *s2)
 	{
-	return (0);
+	if (*s1 != '\0')
+	{
+	return (wildcmp(s1 + 1, s2 + 1));
 	}
+	return (1);
+	}
+	return (0);
 }
