23ebb5d54253de9ce8364c18396661aa6a69e868
Aya <aysa109@gmail.com>
2023-09-01 02:24:00 +0000

    101- wildcompare

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -1,5 +1,4 @@
-#i:wq
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,7 @@
 #include "main.h"
 
 /**
- *
- * Return: 1 Success
- */
-
-
-
-{
-
-
-}
-
-/**
- * wildcmp -  compares two strings and returns 1
+ *  wildcmp - compares two strings and returns 1
  * @s1: first string
  * @s2: second string
  * Return: 1 Success
@@ -21,6 +9,24 @@
 
 int wildcmp(char *s1, char *s2)
 {
-
-
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+	return (1);
+	}
+	else if (*s1 == *s2 || (*s2 == '*' && *(s2 + 1) != '\0' && *s1 != '\0'))
+	{
+	return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*' && *(s2 + 1) == '*')
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	else
+	{
+	return (0);
+	}
 }
