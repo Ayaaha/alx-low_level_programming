e826fc67650307d6448e35c06f47cd37e66d30cc
Aya <aysa109@gmail.com>
2023-09-02 01:08:52 +0000

    0-whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints aprogram name 
+ * main - prints aprogram name
  * @argc: integer
  * @argv: file name
  * Return: 1 Success
  */
 
-int main(char *argv[])
+int main(char const *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
