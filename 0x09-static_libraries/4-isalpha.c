cbbbe2bf83e3e40d82021b142485dd38d9ad2f52
Aya <aysa109@gmail.com>
2023-09-02 00:29:56 +0000

    0-libmy

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to be checked
+ *
+ * Return: 1 Success
+ */
+int _isalpha(int c)
+{
+	if ((c  >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	return (1);
+	else
+	return (0);
+}
+
