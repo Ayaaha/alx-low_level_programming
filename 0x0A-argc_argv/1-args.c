9d86fde75c0b445c0c17be1207d86af89001233f
Aya <aysa109@gmail.com>
2023-09-02 01:21:20 +0000

    1-argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints aprogram name
+ * @argc: integer
+ * @argv: file name
+ * Return: 1 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%d\n", argc);
+	return (0);
+
+}
