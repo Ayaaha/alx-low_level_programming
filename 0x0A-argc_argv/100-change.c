daf6a039e1b7934a2527bec6682797f54d088ecc
Aya <aysa109@gmail.com>
2023-09-02 20:29:20 +0000

    100-coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints multiplication of two integer
+ * main - prints  prints the minimum number of coins
  * @argc: integer
  * @argv: file name
  * Return: 1 Success
@@ -10,19 +10,55 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, mul;
+	int min;
+	int total_change;
+	int changes[5] = {25, 10, 5, 2, 1};
+	min = 0;
 
-	if (argc != 3)
+	if (argc != 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
+
+	total_change = atoi(argv[1]);
+	if (total_change < 0)
+	{
+	printf("0\n");
+	return (0);
+	}
+	for (i = 0; i <= 4; i++)
+	{
+	while (total_change >= changes[i])
+	{
+	else if (total_change >= 25)
+	{
+	total_change = total_change - 25;
+	a++;
+	}
+	else if (total_change >= 10)
+	{
+	total_change = total_change - 10;
+	b++;
+	}
+	else if (total_change >= 5)
+	{
+	total_change = total_change - 5;
+	c++;
+	}
+	else if (total_change >= 2)
+	{
+	total_change = total_change - 2;
+	d++;
+	}
 	else
 	{
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	mul = a * b;
-	printf("%d\n", mul);
+	total_change = total_change - 1;
+	e++;
 	}
+	}
+	min = a + b + c + d + e;
+	printf("%d\n", min);
+	total_change = atoi(argv[0]);
 	return (0);
 }
