760f4807d4b27f76d9b493f7797cd33af8ec7c54
Aya <aysa109@gmail>
2023-09-06 02:35:50 +0000

    4-free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * free_grid - free the previous grid
+ * @**grid: two dimenstional array
+ * @height: the height of the array
+ * Return: 1 Success
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+
+		int i;
+
+	if (height <= 0 || grid == NULL)
+	{
+	return (NULL);
+	}
+	if (height > 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(g[i]);
+		}
+		free(grid);
+	}
+	return (0);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -12,6 +12,9 @@
  * *_strdup - returns a pointer to a newly allocated space in memory
  * *_str_concat - concatenates two strings
  * **alloc_grid -  returns a pointer to a 2 dimensional array of integers
+ * free_grid -  frees a 2 dimensional grid
+ * *argstostr - concatenates all the arguments of your program
+ * **strtow - splits a string into words
  */
 
 int main(void);
@@ -20,5 +23,8 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
