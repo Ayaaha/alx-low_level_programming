ad252e55c50b0a819c22f94f6be3f9256990e53a
Aya <aysa109@gmail>
2023-09-06 00:13:30 +0000

    2-alloc-concatination two strings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,10 +10,12 @@
  * _putchar - prints character in the  output
  * *create_array - creates an array of chars
  * *_strdup - returns a pointer to a newly allocated space in memory
+ * *_str_concat - concatenates two strings
  */
 
 int main(void);
 int _putchar(char ch);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
