80470d76d93c11a5b2841d9e8817590dd28287b0
Aya <aysa109@gmail>
2023-09-07 22:58:19 +0000

    2-allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,49 +1,27 @@
 #include "main.h"
 
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: integer number
+ * _calloc - allocates memory for an array, using malloc
+ * @size: integer number
+ * @nmemb: number of elements
  * Return: 1 Success
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int *array;
 
-	unsigned int i, j, len1, len2, total_len;
-	char *total_string;
-
-	if (s1 == NULL)
+	if (nmemb == 0 || size == 0)
 	{
-	s1 = "";
+	return (NULL);
 	}
-	if (s2 == NULL)
-	{
-	s2 = "";
-	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (n >= len2)
-	total_len = len1 + len2;
-	else
-	total_len = len1 + n;
-	total_string = (char *) malloc((total_len + 1) * sizeof(char));
-	if (total_string == NULL)
+	array = malloc(nmemb * sizeof(int));
+	if (array == NULL)
 	{
 	return (NULL);
 	}
 	else
 	{
-		for (i = 0; i < len1; i++)
-		{
-		total_string[i] = s1[i];
-		}
-		for (j = 0; j < len2; j++)
-		{
-		total_string[i + j] = s2[j];
-		}
-		total_string[total_len] = '\0';
-	return (total_string);
+		return (array);
 	}
 }
