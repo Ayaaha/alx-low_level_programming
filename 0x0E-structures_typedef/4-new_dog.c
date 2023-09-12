2787bba19f1b13a2fcfb984f00382617c8b88e8e
Aya <aysa109@gmail>
2023-09-12 22:43:50 +0000

    4-creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: 1 on (success)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog != NULL)
+	{
+	strcpy(new_dog->name);
+	strcpy(new_dog->owner);
+	}
+	else
+	{
+	return (NULL);
+	}
+}
