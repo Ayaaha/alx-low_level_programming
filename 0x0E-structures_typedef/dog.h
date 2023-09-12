0e63f48a8a9e381c25b9eb95d0ee1586b30e66ee
Aya <aysa109@gmail>
2023-09-12 21:22:39 +0000

    0-dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,18 @@
 #define DOG_H
 #include <stdio.h>
 
+/**
+ * struct dog - structure named dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-
 
 #endif
