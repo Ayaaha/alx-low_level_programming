6e92d2b36e0f182f9399b654eb81103e64a554db
Aya <aysa109@gmail>
2023-09-12 22:12:24 +0000

    2-print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,18 +13,18 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	}
 	if (d == NULL)
 	{
-	printf("");
+	return;
 	}
-	if (name == NULL)
+	if (d->name == NULL)
 	{
-	printf(" Name: (nil)");
+		d->name = "(nil)";
 	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name:%s\nAge: %f\nOwner:%s\n", d->name, d.age, d->owner);
+
 }
